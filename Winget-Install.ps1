1eaf3697d64042091ed2cca34fef720212af4de9
uviiiiiin <uvinamarathunga@borderexpress.com.au>
2026-06-10 14:53:38 +1000

    Update Winget-Install.ps1

--- Winget-Install.ps1
+++ Winget-Install.ps1
@@ -1,7 +1,7 @@
 #set variables
 $WingetAppID = "Microsoft.SQLServerManagementStudio"
 
-#Help SYSTEM find winget.exe folder
+#Set location path so the System runs the app from this location
 Set-Location -Path ("$env:ProgramW6432\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe")
 
 #Install software with winget.exe
